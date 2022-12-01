29d3ce119fb40cbb281acf85061827439f73caf3
CauchyPeano <431522+CauchyPeano@users.noreply.github.com>
2022-12-01 22:35:02 +0100

    first problem solved

---
+++ src/main/kotlin/ProblemOne.kt
@@ -0,0 +1,29 @@
+import kotlin.math.max
+
+fun main(array: Array<String>) {
+
+    val input = getResourceAsText("input1.txt")
+    val acc = mutableListOf<Int>(0)
+
+
+    val folded = input.lines()
+        .foldRight(acc,
+            { s, accp ->
+                if (s.isBlank()) acc.add(0) else acc[acc.size - 1] += s.toInt()
+                accp
+            })
+
+    val maxCalories = folded.max()
+
+    val topThreeMaxCalories = with(folded.sorted().reversed()) {
+        component1() + component2() + component3()
+    }
+
+    println(maxCalories)
+
+    println(topThreeMaxCalories)
+
+}
+
+fun getResourceAsText(path: String): String =
+    object {}.javaClass.getResource(path)!!.readText()
